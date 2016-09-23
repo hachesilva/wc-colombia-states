af6b48b5240220ac3f08cd6ed837f0198ec01c0c
Hernan Silva <hachesilva@gmail.com>
2016-09-23 16:01:24 -0500

    Initial release

---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== WC Colombia States ===
+Contributors: hachesilva
+Donate link: hachesilva@gmail.com
+Tags: woocommerce, states, state, colombia, regions, shipping
+Requires at least: 4.0
+Tested up to: 4.6.1
+Stable tag: 1.0.0
+WC requires at least: 2.2
+WC tested up to: 2.6.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add Colombian states to WooCommerce
+
+== Description ==
+
+A list of colombian states (departamentos) will be added to WooCommerce to be rendered as a drop down list instead of an input text
+
+== Installation ==
+
+1. Upload to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Enjoy
+
+== Changelog ==
+
+= 1.0 =
+* First release.
---
+++ wc-colombia-states.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Plugin Name: Woocommerce Colombia States
+Plugin URI:
+Description: Add Colombian states to WooCommerce
+Version: 1.0
+Author: @hachesilva
+Author URI: http://hachesilva.com
+License: GPL2
+*/
+
+/*
+ * Die if the file is accessed directly
+ */
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*
+ * Check if WooCommerce is active
+ */
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	add_filter( 'woocommerce_states', 'wc_colombia_states' );
+
+	function wc_colombia_states( $states ) {
+		$states['CO'] = array(
+			'AMA' => 'Amazonas',
+			'ANT' => 'Antioquia',
+			'ARA' => 'Arauca',
+			'ATL' => 'Atlántico',
+			'BOL' => 'Bolívar',
+			'BOY' => 'Boyacá',
+			'CAL' => 'Caldas',
+			'CAQ' => 'Caquetá',
+			'CAS' => 'Casanare',
+			'CAU' => 'Cauca',
+			'CES' => 'Cesar',
+			'CHO' => 'Chocó',
+			'COR' => 'Córdoba',
+			'CUN' => 'Cundinamarca',
+			'GUA' => 'Guainía',
+			'GUV' => 'Guaviare',
+			'HUI' => 'Huila',
+			'LAG' => 'La Guajira',
+			'MAG' => 'Magdalena',
+			'MET' => 'Meta',
+			'NAR' => 'Nariño',
+			'NSA' => 'Norte de Santander',
+			'PUT' => 'Putumayo',
+			'QUI' => 'Quindio',
+			'RIS' => 'Risaralda',
+			'SAP' => 'San Andrés y Providencia',
+			'SAN' => 'Santander',
+			'SUC' => 'Sucre',
+			'TOL' => 'Tolima',
+			'VAC' => 'Valle del Cauca',
+			'VAU' => 'Vaupés',
+			'VID' => 'Vichada'
+		);
+
+		return $states;
+	}
+}
